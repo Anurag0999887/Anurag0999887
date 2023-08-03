1ca61ad183acb3c5222e9c065dd0362843e6a7af
Anurag0999887 <141341458+Anurag0999887@users.noreply.github.com>
2023-08-03 20:00:38 +0530

    Create P1.cpp
    Assignment 1 of let's upgrade

---
+++ P1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    if (number % 2 == 0) {
+        std::cout << "Number is even." << std::endl;
+    } else {
+        std::cout << "Number is odd." << std::endl;
+    }
+
+    return 0;
+}
